6607ade08990db7c2ade071537d818681aadee91
Gord Larson <gordlarson@gmail.com>
2014-10-28 16:42:27 -0700

    add tests for match context at start and end of document body

--- models/tests.py
+++ models/tests.py
@@ -61,14 +61,41 @@ class MatchSingletTest(unittest.TestCase):
                                                         match=self.match)
         fmt = u'duck goose raccoon{}鬐鶤鶐膗,觾韄煔垥'
         self.doc.body = fmt.format(self.with_context)
+        self.body = self.doc.body
+        self.singlet = MatchSinglet(file_name=self.file_name,
+                                    passage=self.match,
+                                    document=self.doc)
 
     def test_get_context(self):
         """
         Test that get_context returns the match and appropriate context
         """
-        singlet = MatchSinglet(file_name=self.file_name,
-                               passage=self.match,
-                               document=self.doc)
         pad_chars = len(self.context_pad)
-        match_with_context = singlet.get_context(context_chars=pad_chars)
+        match_with_context = self.singlet.get_context(context_chars=pad_chars)
         self.assertEqual(match_with_context, self.with_context)
+
+        # Test match at end of body
+        loc = self.body.find(self.match)
+        end_match = loc + len(self.match)
+        self.doc.body = self.doc.body[:end_match]
+        end_singlet = MatchSinglet(file_name=self.file_name,
+                                   passage=self.match,
+                                   document=self.doc)
+        end_match_context = end_singlet.get_context(context_chars=pad_chars)
+        self.assertEqual(len(end_match_context), len(self.match)+pad_chars)
+
+        # Test match at start of body
+        self.doc.body = self.body[loc:]
+        beg_singlet = MatchSinglet(file_name=self.file_name,
+                                   passage=self.match,
+                                   document=self.doc)
+        beg_match_context = beg_singlet.get_context(context_chars=pad_chars)
+        self.assertEqual(len(beg_match_context), len(self.match)+pad_chars)
+
+    def test_to_dict(self):
+        """
+        Test conversion to dict representation
+        """
+        sing_dict = self.singlet.to_dict()
+        self.assertEqual(sing_dict['file_name'], self.file_name)
+        self.assertEqual(sing_dict['passage'], self.match)
