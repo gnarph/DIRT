0acab05a4053ae25babdae511a53eed6c9788a44
Gord Larson <gordlarson@gmail.com>
2014-11-05 09:26:26 -0800

    use absolute path for translation data file, fix style errors

--- preprocessing/translation/tests.py
+++ preprocessing/translation/tests.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import codecs
-import json
 import unittest
 
-import translation
+import preprocessing.translation.translation as translation
 
 
 class TranslationTests(unittest.TestCase):
-    
-    def test_translation(self):
-        simplified = u'来自Bandai Namco Games TW/HK 官方FACEBOOK情报，PS3/PSV平台动漫改编作品《刀剑神域：失落之歌》繁体中文版正式公开!预定于2015年发售，并一同公开了繁体中文版宣传片，下面来欣赏了解一下。'.encode('utf8')
-        traditional = u'來自Bandai Namco Games TW/HK 官方FACEBOOK情報，PS3/PSV平台動漫改編作品《刀劍神域：失落之歌》繁體中文版正式公開!預定於2015年發售，並一同公開了繁體中文版宣傳片，下面來欣賞了解一下。'.encode('utf8')
 
-        #translation_dicts = translation.mdic()
+    def test_translation(self):
+        simplified = u'来自Bandai Namco Games TW/HK 官方FACEBOOK情报' \
+                     u'，PS3/PSV平台动漫改编作品《刀剑神域：失落之歌》繁' \
+                     u'体中文版正式公开!预定于2015年发售，并一同公开了繁' \
+                     u'体中文版宣传片，下面来欣赏了解一下。'.encode('utf8')
+        traditional = u'來自Bandai Namco Games TW/HK 官方FACEBOOK情報，' \
+                      u'PS3/PSV平台動漫改編作品《刀劍神域：失落之歌》繁' \
+                      u'體中文版正式公開!預定於2015年發售，並一同公開了繁' \
+                      u'體中文版宣傳片，下面來欣賞了解一下。'.encode('utf8')
+
         [dic_TW,dic_HK,dic_CN] = translation.mdic()
         translated = translation.conv(simplified, dic_TW)
-        self.assertEqual(translated, traditional)   
-        
-
-
-def main():
-    unittest.main()
-
-if __name__ == '__main__':
-    main()
+        self.assertEqual(translated, traditional)
--- preprocessing/translation/translation.py
+++ preprocessing/translation/translation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import io, json
 
-# Maximum Forward Match
-def conv(string,dic):
+def conv(string, dic):
+    """
+    Maximum Forward Match
+    :param string:
+    :param dic:
+    :return:
+    """
     i = 0
     while i < len(string):
         for j in range(len(string) - i, 0, -1):
@@ -15,10 +19,15 @@ def conv(string,dic):
                 break
         i += 1
     return string
- 
-# Translating words according to the dictionary
-def mdic():    
-    table = open('ZhConversion.php','r').readlines()
+
+
+def mdic():
+    """
+    Translate words according to the dictionary
+    :return: ?
+    """
+    with open('preprocessing/translation/ZhConversion.php', 'r') as f:
+        table = f.readlines()
     dic = dict()
     name = []
     for line in table:
@@ -29,19 +38,12 @@ def mdic():
         if line[0] == "'":
             word = line.split("'")
             dic[word[1]] = word[3]
-    name[3].update(name[1]) # Simplified to Traditional(zh2Hant) Taiwanese Style(zh2TW)
-    #name[4].update(name[1]) # Simplified to Traditional(zh2Hant) HongKong Style (zh2HK)
-    #name[5].update(name[2]) # Traditional to Simplified (zh2Hans) Chinese Style(zh2CN)
-    return name[3],name[4],name[5]
- 
-if __name__=="__main__":
-    f = open('workfile.txt', 'r')
-    temp = f.read()
-    [dic_TW,dic_HK,dic_CN] = mdic()
-    str_TW = conv(temp,dic_TW)
-    #str_HK = conv(c,dic_HK)    Translate into Traditional Language(HongKong), another style of Traditional, not the best.
-    #str_CN = conv(b,dic_CN)    Translate into Simplified from Traditional
-    print unicode(temp,'utf8').encode('utf8'), ' <-> ', unicode(str_TW,'utf8').encode('utf8') # Print Simplified on left side and Traditional on right side
 
-    
-    
+    # TODO: should have a param to indicate which dict to return
+    # Simplified to Traditional(zh2Hant) Taiwanese Style(zh2TW)
+    name[3].update(name[1])
+    # Simplified to Traditional(zh2Hant) HongKong Style (zh2HK)
+    #name[4].update(name[1])
+    # Traditional to Simplified (zh2Hans) Chinese Style(zh2CN)
+    #name[5].update(name[2])
+    return name[3], name[4], name[5]
