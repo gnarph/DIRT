df6380a49f5ddff9ef1567e2b23a2d380f2b3f91
Gord Larson <gordlarson@gmail.com>
2014-11-07 14:28:33 -0800

    further refactoring, add documentation to match concatenator

--- processing/comparators/match_concatenator.py
+++ processing/comparators/match_concatenator.py
@@ -20,8 +20,16 @@ def difflib_blocks_to_match_tuples(blocks):
 
 
 class MatchConcatenator(object):
+    """
+    Class for combining match blocks based on their locations
+    and sizes, covered by gap_length
+    """
 
     def __init__(self, match_list, gap_length):
+        """
+        :param match_list: list of MatchTuples
+        :param gap_length: max length of gap to jump
+        """
         self.match_list = match_list
         self.i = 0
         self.j = 1
@@ -38,6 +46,12 @@ class MatchConcatenator(object):
         self.b_cursor = second.b_end
 
     def _process_main(self):
+        """
+        Process the blocks (except the last)
+        :return: leftmost block not combined,
+                 rightmost block combining,
+                 should we continue using process main
+        """
         first = self.match_list[self.i]
         second = self.match_list[self.j]
         if not self.jump_gap(second):
@@ -55,10 +69,19 @@ class MatchConcatenator(object):
         return first, second, cont
 
     def combine_and_select_block(self, first):
-        block = self.get_block(first)
+        """
+        Combine block and add it to combined list
+        :param first: block at which to start the combination
+        """
+        block = self.combine_block(first)
         self.combined.append(block)
 
     def _process_last(self, first, second):
+        """
+        Processes the last match block
+        :param first:
+        :param second:
+        """
         last = second
         if not self.jump_gap(last):
             if first.a_end == self.a_cursor:
@@ -75,6 +98,10 @@ class MatchConcatenator(object):
             self.combine_and_select_block(first)
 
     def concatenate(self):
+        """
+        Concatenate match blocks depending on gap_length
+        :return: list of concatenated blocks
+        """
         cont = True
         while cont:
             first, second, cont = self._process_main()
@@ -86,13 +113,23 @@ class MatchConcatenator(object):
         return (self.a_cursor > first.a and
                 self.b_cursor > first.b)
 
-    def get_block(self, first):
+    def combine_block(self, first):
+        """
+        Combine from first block to cursors
+        :param first: position to start combining
+        :return: combined block
+        """
         return MatchTuple(a=first.a,
                           b=first.b,
                           a_end=self.a_cursor,
                           b_end=self.b_cursor)
 
     def jump_gap(self, last):
+        """
+        Check if we can jump from the cursors to a match
+        :param last: match to attempt a jump to
+        :return: if we can jump to the next match
+        """
         a_gap = last.a - self.a_cursor
         b_gap = last.b - self.b_cursor
         return (a_gap <= self.gap_length and
