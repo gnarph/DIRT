f3ef0ef7c5c70f3fa8418a52bb18db9212b97435
Gord Larson <gordlarson@gmail.com>
2015-01-20 14:38:38 -0800

    remove unused code

--- models/match_set.py
+++ models/match_set.py
@@ -1,6 +1,5 @@
 from models.match import Match
 from models.document import Document
-from utilities import file_ops
 
 
 class MatchSet(object):
@@ -33,11 +32,6 @@ class MatchSet(object):
                 'beta_doc': self.beta_doc.file_name,
                 'matches': [match.to_dict() for match in self.matches],
                 }
-
-    @staticmethod
-    def from_json(filename):
-        json = file_ops.read_json_utf8(filename)
-        return MatchSet.from_dict(json)
 
     @staticmethod
     def from_dict(d):
