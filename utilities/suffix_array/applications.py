ff5a03811c60b192f7801bd365275fd88e9e67a0
Gord Larson <gordlarson@gmail.com>
2015-01-19 17:36:21 -0800

    add more suffix array tests

--- utilities/suffix_array/applications.py
+++ utilities/suffix_array/applications.py
@@ -6,13 +6,17 @@ by Yubin Bai
 import utilities.suffix_array.tools_karkkainen_sanders as tks
 
 
+class InvalidCharacterException(Exception):
+    pass
+
+
 def all_common_substrings(a, b, separator='$'):
     """
     all substrings in both a and b
     such that no substring is a substring of another substring
     """
     if separator in a or separator in b:
-        raise Exception('Separator in input strings')
+        raise InvalidCharacterException('Separator in input strings')
     ab = u''.join([a, separator, b])
     sa = tks.simple_kark_sort(ab)
     lcp = tks.LCP(ab, sa)
@@ -22,29 +26,31 @@ def all_common_substrings(a, b, separator='$'):
     for i, v in enumerate(lcp):
         # Only want something that is present in
         # a AND b, not a AND a or b AND b
-        passage = ab[sa[i]:sa[i] + v]
+        start = sa[i]
+        end = start + v
+        p = ab[start:end]
         # Checking passage in a|b in case the passage occurs twice in
         # a or twice in b
-        if (sa[i] > sep and passage in a) or (sa[i]+v <= sep and passage in b):
-            if passage not in all_subs:
+        if (start > sep and p in a) or (end <= sep and p in b):
+            if p not in all_subs:
                 to_remove = set()
                 take = True
                 for s in all_subs:
                     # Current strategy aims to keep memory use
                     # lower, could go faster if this filtering
                     # was done after the fact
-                    if passage in s:
+                    if p in s:
                         # If we p is a substring of s
                         # we don't want to take it so we can leave
                         take = False
                         break
-                    elif s in passage:
+                    elif s in p:
                         # We want to remove s if it is superceded
                         # by p
                         to_remove.add(s)
                 all_subs -= to_remove
                 if take:
-                    all_subs.add(passage)
+                    all_subs.add(p)
 
     return all_subs
 
--- utilities/suffix_array/test.py
+++ utilities/suffix_array/test.py
@@ -1,11 +1,22 @@
 import unittest
 
 from utilities.suffix_array import applications as app
+from utilities import file_ops
+
+
+def strip_set(s):
+    new_set = set()
+    for i in s:
+        strip = i.strip(u' \t\n\r')
+        r = strip.replace(u'\n', u'')
+        r = r.replace(u'.', u'')
+        new_set.add(r)
+    return new_set
 
 
 class SuffixArrayApplicationTest(unittest.TestCase):
 
-    def testACS(self):
+    def test_acs_small(self):
         a = u'jeffisacoolguywhoiscool'
         b = u'whoiscooljeffisacoolguy'
         acs = app.all_common_substrings(a, b)
@@ -20,3 +31,40 @@ class SuffixArrayApplicationTest(unittest.TestCase):
         self.assertIn(u'bcc', acs)
         self.assertIn(u'ab', acs)
         self.assertEqual(len(acs), 3)
+
+        a = u'sallyjohndeer'
+        b = u'johnnyrobinpear'
+        c = u'chairlairjohn'
+        ab = app.all_common_substrings(a, b)
+        ac = app.all_common_substrings(a, c)
+        bc = app.all_common_substrings(b, c)
+        in_all = ab & ac & bc
+        self.assertIn(in_all, u'john')
+
+    def test_acs_except(self):
+        a = u'dolla$bills'
+        b = u'yo'
+        execpt = app.InvalidCharacterException
+        to_call = app.all_common_substrings
+        self.assertRaises(execpt, to_call, a, b)
+
+    def test_acs_large(self):
+        f_one = 'test_data/full_test/one.txt'
+        f_two = 'test_data/full_test/two.txt'
+        f_three = 'test_data/full_test/three.txt'
+
+        one = file_ops.read_utf8(f_one)
+        two = file_ops.read_utf8(f_two)
+        three = file_ops.read_utf8(f_three)
+
+        one_two = app.all_common_substrings(one, two)
+        one_three = app.all_common_substrings(one, three)
+        two_three = app.all_common_substrings(two, three)
+
+        # Strip is somewhat hacky
+        strip_a = strip_set(one_two)
+        strip_b = strip_set(one_three)
+        strip_c = strip_set(two_three)
+
+        in_all = strip_a & strip_b & strip_c
+        self.assertEqual(len(in_all), 1)
