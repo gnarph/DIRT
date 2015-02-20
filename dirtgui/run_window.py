c2a955323732b5e9cbe58a1d5923e18fa98711dc
Johnny Luk <jokerking01@gmail.com>
2015-02-19 17:18:52 -0800

    Layout
    Added in box layout

--- dirtgui/run_window.py
+++ dirtgui/run_window.py
@@ -10,23 +10,38 @@ class RunningWindow(QMainWindow):
 
     def __init__(self):
         QMainWindow.__init__(self)
-        layout = QHBoxLayout()
-        button = QPushButton('DiRT Start!', self)
-        layout.addWidget(button)
 
-        self.widget = QWidget()
-        self.widget.setLayout(layout)
+        self.layout = BoxLayout(self)
+        self.setCentralWidget(self.layout)
 
-        self.setCentralWidget(self.widget)
-        self.setWindowTitle("Win1")
-
-        self.connect(button, SIGNAL('clicked()'), self.newWindow)
+        self.setGeometry(300, 300, 350, 350)
+        self.setWindowTitle('DiRT Startup')    
         self.show()
+
+
+class BoxLayout(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        
+        self.setWindowTitle('box layout')
+
+        startButton = QtGui.QPushButton("DiRT Start")
+        
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(startButton)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        self.connect(startButton, SIGNAL('clicked()'), self.newWindow)
+
 
     def newWindow(self):
         self.other_window = MainWindow()
         self.other_window.show()
-
 
 def main():
     app = QtGui.QApplication(sys.argv)
